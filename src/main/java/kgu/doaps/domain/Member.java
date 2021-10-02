c3193cc0347bbae1a509cec9f236a3830a3aa38f
khs <69028828+happysang@users.noreply.github.com>
2021-10-02 17:28:21 +0900

    Member, Delivery 도메인 작성

--- src/main/java/kgu/doaps/domain/Member.java
+++ src/main/java/kgu/doaps/domain/Member.java
@@ -1,5 +1,23 @@
 package kgu.doaps.domain;
 
-public class Member {
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Member {
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private String id;
+
+    private String pwd;
+
+    private String name;
+
+    private MemberStatus memberStatus;
+
+    @Embedded
+    private Address address;
 }
