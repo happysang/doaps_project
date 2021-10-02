fa16d98d7ea11c94575da5a665d24fed945c9ccd
khs <69028828+happysang@users.noreply.github.com>
2021-10-02 15:04:30 +0900

    프로젝트생성

---
+++ src/main/java/kgu/doaps/DoapsApplication.java
@@ -0,0 +1,13 @@
+package kgu.doaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoapsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kgu/doaps/DoapsApplicationTests.java
@@ -0,0 +1,13 @@
+package kgu.doaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
