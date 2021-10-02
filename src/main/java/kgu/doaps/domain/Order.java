95f561584c9ac6c723b45f1bdc1de8b558cbd8a0
khs <69028828+happysang@users.noreply.github.com>
2021-10-02 17:41:48 +0900

    Item, OrderItem 도메인 작성

--- src/main/java/kgu/doaps/domain/Order.java
+++ src/main/java/kgu/doaps/domain/Order.java
@@ -1,15 +1,19 @@
 package kgu.doaps.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "orders")
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
     @Id @GeneratedValue
@@ -22,4 +26,13 @@ public class Order {
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItemLists = new ArrayList<>();
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
 }
