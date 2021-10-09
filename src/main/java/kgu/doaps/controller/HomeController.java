727c981e10c75bb2ffed52dc8b77eb82fc55782b
khs <69028828+happysang@users.noreply.github.com>
2021-10-09 17:48:45 +0900

    .
    .

---
+++ src/main/java/kgu/doaps/controller/HomeController.java
@@ -0,0 +1,14 @@
+package kgu.doaps.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        return "home";
+    }
+}
