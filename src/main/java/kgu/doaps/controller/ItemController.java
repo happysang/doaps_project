9e3c4b93c827c943fdc4dec3e0fec22f3cb80dae
khs <69028828+happysang@users.noreply.github.com>
2021-10-10 14:57:31 +0900

    기초완성

--- src/main/java/kgu/doaps/controller/ItemController.java
+++ src/main/java/kgu/doaps/controller/ItemController.java
@@ -7,10 +7,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.awt.print.Book;
 import java.util.List;
 
 @Controller
@@ -21,19 +21,19 @@ public class ItemController {
 
     @GetMapping("/items/new")
     public String createForm(Model model) {
-        model.addAttribute("form", new BookForm());
+        model.addAttribute("form", new PepperForm());
         return "items/createItemForm";
     }
 
     @PostMapping("/items/new")
-    public String create(BookForm form) {
+    public String create(PepperForm form) {
         Pepper pepper = new Pepper();
         pepper.setName(form.getName());
         pepper.setPrice(form.getPrice());
         pepper.setStockQuantity(form.getStockQuantity());
 
         itemService.saveItem(pepper);
-        return "redirect:/";
+        return "redirect:/items";
     }
 
     @GetMapping("/items")
@@ -47,7 +47,7 @@ public class ItemController {
     public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
         Pepper item = (Pepper) itemService.findOne(itemId);
 
-        BookForm form = new BookForm();
+        PepperForm form = new PepperForm();
         form.setId(item.getId());
         form.setName(item.getName());
         form.setPrice(item.getPrice());
@@ -56,5 +56,16 @@ public class ItemController {
         model.addAttribute("form", form);
         return "items/updateItemForm";
     }
-    //update는 많이 얽혀 있어서 추후에..ㅜ
+
+    @PostMapping(value = "/items/{itemId}/edit")
+    public String updateItem(@ModelAttribute("form") PepperForm form) {
+        Pepper pepper = new Pepper();
+        pepper.setId(form.getId());
+        pepper.setName(form.getName());
+        pepper.setPrice(form.getPrice());
+        pepper.setStockQuantity(form.getStockQuantity());
+        itemService.saveItem(pepper);
+        return "redirect:/items";
+    }
+
 }
--- src/main/java/kgu/doaps/controller/PepperForm.java
+++ src/main/java/kgu/doaps/controller/PepperForm.java
@@ -4,11 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
-public class BookForm {
+public class PepperForm {
     private Long id;
     private String name;
     private int price;
     private int stockQuantity;
-    private String author;
-    private String isbn;
 }
