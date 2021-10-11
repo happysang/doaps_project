ee520696f40b99c8f08684105356a0eb2a1e2e03
khs <69028828+happysang@users.noreply.github.com>
2021-10-11 22:19:36 +0900

    회원가입 작성, 로그인 에러

---
+++ src/main/java/kgu/doaps/TestDataInit.java
@@ -0,0 +1,26 @@
+package kgu.doaps;
+
+import kgu.doaps.domain.Address;
+import kgu.doaps.domain.Member;
+import kgu.doaps.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@RequiredArgsConstructor
+public class TestDataInit {
+
+    private final MemberService memberService;
+
+    @PostConstruct
+    public void init(){
+        Member member = new Member();
+        member.setLoginId("id1");
+        member.setPassword("1234");
+        member.setName("name1");
+        member.setAddress(new Address("서울시", "송파구", "방이동"));
+        memberService.join(member);
+    }
+}
---
+++ src/main/java/kgu/doaps/service/LoginService.java
@@ -0,0 +1,22 @@
+package kgu.doaps.service;
+
+import kgu.doaps.domain.Member;
+import kgu.doaps.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+    private final MemberRepository memberRepository;
+
+
+    public Member login(String memberId, String password) {
+        Member member = (Member) memberRepository.findByLoginId(memberId);
+        if (member.getPassword().equals(password)){
+            return member;
+        } else {
+            return null;
+        }
+    }
+}
