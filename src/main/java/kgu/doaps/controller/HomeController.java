67035e07ffab04236d710feadc64ea98103baed8
khs <69028828+happysang@users.noreply.github.com>
2021-10-16 04:05:02 +0900

    세션 사용하여 로그인

--- src/main/java/kgu/doaps/controller/HomeController.java
+++ src/main/java/kgu/doaps/controller/HomeController.java
@@ -2,38 +2,35 @@ package kgu.doaps.controller;
 
 import kgu.doaps.domain.Member;
 import kgu.doaps.repository.MemberRepository;
+import kgu.doaps.web.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.dom4j.rule.Mode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @Slf4j
 @RequiredArgsConstructor
 public class HomeController {
 
-    private final MemberRepository memberRepository;
-//    @RequestMapping("/")
-//    public String home() {
-//        log.info("home controller");
-//        return "home";
-//    }
+    private final SessionManager sessionManager;
 
     @RequestMapping("/")
-    public String home(@CookieValue(name = "memberId", required = false) Long memberId, Model model){
-        if (memberId == null){
-            return "home";
-        }
+    public String home(HttpServletRequest request, Model model){
+        //세션 관리자에 저장된 회원 정보 조회
+        Member member = (Member)sessionManager.getSession(request);
 
         //로그인
-        Member loginMember = memberRepository.findById(memberId);
-        if (loginMember == null){
+        if (member == null){
             return "home";
         }
 
-        model.addAttribute("member", loginMember);
+        model.addAttribute("member", member);
         return "loginHome";
     }
 }
