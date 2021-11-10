fd5eb50de88586d1944dbd9249f5a749d2f07da9
khs <69028828+happysang@users.noreply.github.com>
2021-11-10 16:03:15 +0900

    내정보수정 완료

--- src/main/java/kgu/doaps/controller/MemberUpdateForm.java
+++ src/main/java/kgu/doaps/controller/MemberUpdateForm.java
@@ -8,10 +8,6 @@ import javax.validation.constraints.NotEmpty;
 @Getter @Setter
 public class MemberUpdateForm {
     private Long id;
-
-
-//    @NotEmpty(message = "비밀번호 입력은 필수 입니다")
-//    private String password;
     @NotEmpty(message = "회원 이름 입력은 필수 입니다")
     private String name;
     @NotEmpty(message = "주소 입력은 필수 입니다")
@@ -20,4 +16,6 @@ public class MemberUpdateForm {
     private String street;
     @NotEmpty(message = "주소 입력은 필수 입니다")
     private String zipcode;
+    private String phone;
+
 }
