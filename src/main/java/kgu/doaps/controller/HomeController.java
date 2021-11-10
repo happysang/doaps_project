6cb834dbc7baaf2bd42d83bbad69bc75d7d7aa0a
Lee SeokGeun <gkwoap@nate.com>
2021-11-10 16:43:07 +0900

    헤더분리

--- src/main/java/kgu/doaps/controller/HomeController.java
+++ src/main/java/kgu/doaps/controller/HomeController.java
@@ -3,7 +3,7 @@ package kgu.doaps.controller;
 import kgu.doaps.domain.Member;
 import kgu.doaps.domain.item.Item;
 import kgu.doaps.service.ItemService;
-import kgu.doaps.session.SessionConst;
+import kgu.doaps.session.Login;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
-import javax.validation.constraints.Null;
 import java.util.List;
 
 @Controller
@@ -22,17 +21,15 @@ public class HomeController {
     private final ItemService itemService;
 
     @GetMapping("/")
-    public String home(
-            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model){
-
+    public String home(@Login Member loginMember, Model model){
 
         List<Item> items = itemService.findItems();
-        Member nullMember = new Member();
+
         model.addAttribute("items", items);
 
         //세션에 회원 데이터가 없으면 아무것도 없는 member 만든 후 리턴
         if (loginMember == null){
-            model.addAttribute("member", nullMember);
+            model.addAttribute("member", new Member());
             return "home";
         }
 
